4a12433a21079108667ce94a1524e2478f3f3ae7
LucaSk-x <lucapiccinno64@gmail.com>
2022-06-01 17:46:17 +0200

    completed

--- app.js
+++ app.js
@@ -2,59 +2,65 @@ let colsContainer = document.getElementById("colsContainer");
 
 const teamComponent = [
     {
-        nome: "Wayne",
-        cognome: "Barnett",
+        nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
+        imgSrc: "wayne-barnett-founder-ceo.jpg",
     },
     {
-        nome: "Angela",
-        cognome: "Carroll",
+        nome: "Angela Carroll",
         ruolo: "Chief Editor",
+        imgSrc: "angela-caroll-chief-editor.jpg",
     },
     {
-        nome: "Walter",
-        cognome: "Gordon",
+        nome: "Walter Gordon",
         ruolo: "Office Menager",
+        imgSrc: "walter-gordon-office-manager.jpg",
     },
     {
-        nome: "Angela",
-        cognome: "Lopez",
+        nome: "Angela Lopez",
         ruolo: "Social Media Menager",
+        imgSrc: "angela-lopez-social-media-manager.jpg",
+
     },
     {
-        nome: "Scott",
-        cognome: "Estrada",
+        nome: "Scott Estrada",
         ruolo: "Developer",
+        imgSrc: "scott-estrada-developer.jpg",
     },
     {
-        nome: "Barbara",
-        cognome: "Ramos",
+        nome: "Barbara Ramos",
         ruolo: "Graphic Design",
+        imgSrc: "barbara-ramos-graphic-designer.jpg",
     },
 ];
 
-for (let i = 0; i < teamComponent.length; i++) {
+function createTeam (oggetto) {
+    const myCard = document.createElement("div");
+    myCard.classList.add("my-card", "col-4");
 
-    const create = document.createElement("div");
-    colsContainer.append(create);
-    let colFour = create.classList.add("col-4");
+    const imgContain = document.createElement("div");
+    imgContain.classList.add("img-contain");
 
+    const img = document.createElement("img");
+    img.src = `img/${oggetto.imgSrc}`;
+           /* `img/${object.imgUrl}`; */
 
+    const cardText = document.createElement('div');
+    cardText.classList.add('card-text');
+    cardText.innerHTML = `<h3>${oggetto.nome}</h3><p>${oggetto.ruolo}</p>`;
 
-    let colCard = colsContainer.querySelectorAll(':scope > div');
-    let divCard = document.createElement("div");
-    colCard.append(divCard)
-    let myCard = divCard.classList.add("myCard")
+    imgContain.appendChild(img);
+    myCard.appendChild(imgContain);
+    myCard.appendChild(cardText);
 
-
-    for (let key in teamComponent) {
-        teamComponent[key],
-    
-
-
-        console.log(teamComponent[key].nome);
-        console.log(teamComponent[key].cognome);
-        console.log(teamComponent[key].ruolo);
-    }
-    
+    return myCard
 }
+
+
+function createContain (array, input) {
+    for (let i = 0; array.length; i++){
+        input.appendChild(createTeam(array[i]))
+    }
+}
+
+createContain(teamComponent, colsContainer)
